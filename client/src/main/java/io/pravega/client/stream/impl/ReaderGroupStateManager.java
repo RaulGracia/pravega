5163ac67a09d99bb9f3d7fff2f40ad026c8769c0
Raúl Gracia <raul.gracia@emc.com>
2019-04-18 15:30:59 +0200

    Issue 3645: Leaving changes related only with the proper shutdown of EventProcessorCell.
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>

--- client/src/main/java/io/pravega/client/stream/impl/ReaderGroupStateManager.java
+++ client/src/main/java/io/pravega/client/stream/impl/ReaderGroupStateManager.java
@@ -131,25 +131,19 @@ public class ReaderGroupStateManager {
      * @param lastPosition The last position the reader successfully read from.
      */
     static void readerShutdown(String readerId, Position lastPosition, StateSynchronizer<ReaderGroupState> sync) {
-        log.info("Entering readerShutdown for reader {}", readerId);
         sync.updateState((state, updates) -> {
-            log.info("Shutting down reader {}", readerId);
             Set<Segment> segments = state.getSegments(readerId);
             if (segments == null) {
                 return;
             }
-            log.info("Removing reader {} from reader group. CurrentState is: {}", readerId, state);
+            log.debug("Removing reader {} from reader group. CurrentState is: {}. Position is: {}.", readerId, state, lastPosition);
             if (lastPosition != null && !lastPosition.asImpl().getOwnedSegments().containsAll(segments)) {
-                log.error("Exception removing reader {} from reader group. CurrentState is: {}", readerId, state);
                 throw new IllegalArgumentException(
                         "When shutting down a reader: Given position does not match the segments it was assigned: \n"
                                 + segments + " \n vs \n " + lastPosition.asImpl().getOwnedSegments());
             }
-            log.info("Before updating the ReaderGroupState readerId: {}, position: {}", readerId, lastPosition);
             updates.add(new RemoveReader(readerId, lastPosition == null ? null : lastPosition.asImpl().getOwnedSegmentsWithOffsets()));
-            log.info("After updating the ReaderGroupState readerId: {}, position: {}", readerId, lastPosition);
         });
-        log.info("Exiting readerShutdown for reader {}", readerId);
     }
     
     void close() {
