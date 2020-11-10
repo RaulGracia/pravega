ac84ad4ffe43b566b0cc518bb3f0d07228baf05a
Raúl Gracia <raul.gracia@emc.com>
2020-11-10 17:40:43 +0100

    Ported changes to r0.7
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>

--- segmentstore/storage/impl/src/main/java/io/pravega/segmentstore/storage/impl/bookkeeper/BookKeeperLogFactory.java
+++ segmentstore/storage/impl/src/main/java/io/pravega/segmentstore/storage/impl/bookkeeper/BookKeeperLogFactory.java
@@ -12,12 +12,21 @@ package io.pravega.segmentstore.storage.impl.bookkeeper;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import io.pravega.common.Exceptions;
+import io.pravega.common.Timer;
 import io.pravega.segmentstore.storage.DataLogNotAvailableException;
 import io.pravega.segmentstore.storage.DurableDataLog;
 import io.pravega.segmentstore.storage.DurableDataLogException;
 import io.pravega.segmentstore.storage.DurableDataLogFactory;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
+
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.bookkeeper.client.BookKeeper;
@@ -26,6 +35,8 @@ import org.apache.bookkeeper.conf.ClientConfiguration;
 import org.apache.bookkeeper.net.CommonConfigurationKeys;
 import org.apache.curator.framework.CuratorFramework;
 
+import javax.annotation.concurrent.GuardedBy;
+
 /**
  * Factory for BookKeeperLogs.
  */
@@ -33,11 +44,22 @@ import org.apache.curator.framework.CuratorFramework;
 public class BookKeeperLogFactory implements DurableDataLogFactory {
     //region Members
 
+    // Period of inspection to meet the maximum number of log creation attempts for a given container.
+    private static final Duration LOG_CREATION_INSPECTION_PERIOD = Duration.ofSeconds(60);
+    // Maximum number of log creation attempts for a given container before considering resetting the BK client.
+    private static final int MAX_CREATE_ATTEMPTS_PER_LOG = 2;
+
     private final String namespace;
     private final CuratorFramework zkClient;
     private final AtomicReference<BookKeeper> bookKeeper;
     private final BookKeeperConfig config;
     private final ScheduledExecutorService executor;
+    @Getter(AccessLevel.PACKAGE)
+    @GuardedBy("this")
+    private final Map<Integer, LogInitializationRecord> logInitializationTracker = new HashMap<>();
+    @GuardedBy("this")
+    @Getter(AccessLevel.PACKAGE)
+    private final AtomicReference<Timer> lastBookkeeperClientReset = new AtomicReference<>(new Timer());
 
     //endregion
 
@@ -102,6 +124,7 @@ public class BookKeeperLogFactory implements DurableDataLogFactory {
     @Override
     public DurableDataLog createDurableDataLog(int logId) {
         Preconditions.checkState(this.bookKeeper.get() != null, "BookKeeperLogFactory is not initialized.");
+        tryResetBookkeeperClient(logId);
         return new BookKeeperLog(logId, this.zkClient, this.bookKeeper.get(), this.config, this.executor);
     }
 
@@ -113,6 +136,7 @@ public class BookKeeperLogFactory implements DurableDataLogFactory {
      */
     public DebugLogWrapper createDebugLogWrapper(int logId) {
         Preconditions.checkState(this.bookKeeper.get() != null, "BookKeeperLogFactory is not initialized.");
+        tryResetBookkeeperClient(logId);
         return new DebugLogWrapper(logId, this.zkClient, this.bookKeeper.get(), this.config, this.executor);
     }
 
@@ -167,5 +191,62 @@ public class BookKeeperLogFactory implements DurableDataLogFactory {
         return new BookKeeper(config);
     }
 
+    public void tryResetBookkeeperClient(int logId) {
+        synchronized (this) {
+            if (logInitializationTracker.containsKey(logId)) {
+                // Account for a restart of the Bookkeeper log.
+                logInitializationTracker.get(logId).incrementLogCreations();
+                // If the number of restarts for a single container is meets the threshold, let's reset the BK client.
+                if (logInitializationTracker.get(logId).isBookkeeperClientResetNeeded()
+                        && lastBookkeeperClientReset.get().getElapsed().compareTo(LOG_CREATION_INSPECTION_PERIOD) > 0) {
+                    try {
+                        close();
+                        initialize();
+                        lastBookkeeperClientReset.set(new Timer());
+                    } catch (Exception e) {
+                        log.error("Failure resetting the Bookkeeper client: ", e);
+                        throw new RuntimeException("Unable to reset BookKeeper client.", e);
+                    }
+                }
+            } else {
+                logInitializationTracker.put(logId, new LogInitializationRecord());
+            }
+        }
+    }
+
     //endregion
+
+    /**
+     * Keeps track of the number of log creation attempts within an inspection period.
+     */
+    static class LogInitializationRecord {
+        private final AtomicReference<Timer> timer = new AtomicReference<>(new Timer());
+        private final AtomicInteger counter = new AtomicInteger(0);
+
+        /**
+         * Returns whether the Bookkeeper client should be reset based on the max allowed attempts of re-creating a
+         * log within the inspection period.
+         *
+         * @return whether to re-create the Bookkeeper client or not.
+         */
+        boolean isBookkeeperClientResetNeeded() {
+            return timer.get().getElapsed().compareTo(LOG_CREATION_INSPECTION_PERIOD) < 0 && counter.get() >= MAX_CREATE_ATTEMPTS_PER_LOG;
+        }
+
+        /**
+         * Increments the counter for log restarts within a particular inspection period. If the las sample is older
+         * than the inspection period, the timer and the counter are reset.
+         */
+        void incrementLogCreations() {
+            // If the time since the last log creation is too far, we need to refresh the timer to the new inspection
+            // period and set the counter of log creations to 1.
+            if (timer.get().getElapsed().compareTo(LOG_CREATION_INSPECTION_PERIOD) > 0) {
+                timer.set(new Timer());
+                counter.set(1);
+            } else {
+                // Otherwise, just increment the counter.
+                counter.incrementAndGet();
+            }
+        }
+    }
 }
